9964b96ae97688b0528a669b44f5c07e8743555b
Mateusz <mateusz.warynycia@brokeria.pl>
2022-03-26 20:25:44 +0100

    Menu poprawione klasy

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,16 +1,16 @@
 <template>
     <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0 background-gdansk">
-        <div v-if="canLogin" class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
-            <inertia-link v-if="$page.props.user" href="/dashboard" class="text-sm text-gray-700 underline">
-                Dashboard
+        <div v-if="canLogin" class="hidden fixed top-0 px-6 py-4 sm:block">
+            <inertia-link v-if="$page.props.user" href="/points" class="text-sm text-white text-2xl">
+                Placówki
             </inertia-link>
 
             <template v-else>
-                <inertia-link :href="route('login')" class="text-sm text-white underline">
+                <inertia-link :href="route('login')" class="text-sm text-white text-2xl mr-5">
                     Logowanie
                 </inertia-link>
 
-                <inertia-link v-if="canRegister" :href="route('register')" class="ml-4 text-sm text-white underline">
+                <inertia-link v-if="canRegister" :href="route('register')" class="text-sm text-white text-2xl">
                     Rejestracja
                 </inertia-link>
             </template>
