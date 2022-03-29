8fc22117adbef895f72602eee8edf088afd16d24
lliss <lukasz.liss@brokeria.pl>
2022-03-29 17:22:03 +0200

    Szkic panelu admina

---
+++ app/Http/Controllers/Admin/UserController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\UpdateUserRequest;
+use App\Models\User;
+use Inertia\Inertia;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $users = User::query()
+            ->orderBy('surname')
+            ->orderBy('name')
+            ->paginate();
+
+        return Inertia::render('Admin/User/Index', [
+            'users' => $users,
+        ]);
+    }
+
+
+
+    /**
+     * Display the specified resource.
+     *
+     * @param \App\Models\User $user
+     * @return \Illuminate\Http\Response
+     */
+    public function show(User $user)
+    {
+        //
+    }
+
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param \App\Models\User $user
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(User $user)
+    {
+        //
+    }
+
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param \App\Http\Requests\UpdateUserRequest $request
+     * @param \App\Models\User $user
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateUserRequest $request, User $user)
+    {
+        //
+    }
+
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param \App\Models\User $user
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(User $user)
+    {
+        //
+    }
+}
---
+++ resources/js/Admin/Pages/User/Index.vue
@@ -0,0 +1,33 @@
+<template>
+  <app-admin-layout>
+    <template #header>
+      <h2 class="font-semibold text-2xl text-gray-800 leading-tight">
+        Użytkownicy
+      </h2>
+    </template>
+
+    <div class="container mx-auto">
+      <breadcrumbs/>
+
+
+      <pre class="bg-white">{{ $page.props }}</pre>
+
+    </div>
+  </app-admin-layout>
+</template>
+
+<script>
+import PointCardComponent from '@/Pages/Point/components/PointCardCompoment.vue';
+import Breadcrumbs from '@/Pages/Breadcrumbs.vue';
+import AppAdminLayout from '@/Admin/Layouts/AppAdminLayout.vue';
+
+export default {
+  props: ['sessions'],
+
+  components: {
+    AppAdminLayout,
+    Breadcrumbs,
+    PointCardComponent,
+  },
+}
+</script>
